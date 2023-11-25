1ce2abcf7d47b5b58f81c06b59d063879d3529c4
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2023-11-25 13:16:18 +0700

    add animations and configuration

--- js/script.js
+++ js/script.js
@@ -18,3 +18,17 @@ document.querySelectorAll('li').forEach(n => n.addEventListener("click", () => {
     toggleIcon.classList = isOpen ? "fas fa-times" : "fas fa-bars";
 }))
 
+// animtion
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
